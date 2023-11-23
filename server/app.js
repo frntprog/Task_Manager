059da489985d509a1f20db3f377b5625700f5e0d
frntprog <frntprog@gmail.com>
2023-11-23 19:22:30 +0200

    done api

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 const app = express();
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 
 // middleware
@@ -14,7 +15,9 @@ app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
 
-const port = 3000;
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
